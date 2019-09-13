3b6e33179f39947f13c0376f291136abb5443309
Kareem H <kareemgan@gmail.com>
2019-09-13 11:19:38 -0500

    Update package

--- pgmngr/utils.go
+++ pgmngr/utils.go
@@ -3,26 +3,34 @@ package pgmngr
 import (
 	"database/sql"
 	"fmt"
+	"strconv"
 	"time"
+
+	"github.com/ParaServices/errgo"
 )
 
 func pingDatabase(db *sql.DB, cfg Config) error {
+	var err error
 	for i := 0; i < cfg.Connection.PingIntervals; i++ {
-		err := db.Ping()
+		err = db.Ping()
 		if err == nil {
 			return err
 		}
 		time.Sleep(time.Second)
 	}
-	return NewError(
-		fmt.Errorf(
-			"failed to ping the database: %s on host: %s:%v after %v seconds",
-			cfg.Connection.Database,
-			cfg.Connection.Host,
-			cfg.Connection.Port,
-			cfg.Connection.PingIntervals,
-		),
+	errx := errgo.New(err)
+	errx.Message = fmt.Sprintf(
+		"failed to ping the database: %s on host: %s:%v after %v seconds",
+		cfg.Connection.Database,
+		cfg.Connection.Host,
+		cfg.Connection.Port,
+		cfg.Connection.PingIntervals,
 	)
+	errx.Details.Add("database", cfg.Connection.Database)
+	errx.Details.Add("host", cfg.Connection.Host)
+	errx.Details.Add("port", strconv.Itoa(cfg.Connection.Port))
+	errx.Details.Add("ping_time", strconv.Itoa(cfg.Connection.PingIntervals))
+	return errx
 }
 
 // SliceExclusionInts returns
